0e6d1cd5dc5d0e9cc0cbcc9f1889963a823c2bf5
Mariano <marianonunezma@gmail.com>
2022-08-30 11:20:08 -0300

    Styles Fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,12 +40,12 @@ const Header = () => {
             <div className="md:hidden cursor-pointer" onClick={handleNav}>
                 <AiOutlineMenu size={25}/>
             </div>
-            <div className={navState ? "fixed left-0 top-0 w-full h-screen bg-black/70" : "hidden"}>
+            <div className={navState ? "fixed left-0 top-0 w-full h-screen bg-black/70 ease-in duration-300" : "absolute"}>
                 <div className={navState ? 
                                 "md:hidden fixed left-0 top-0 w-[75%] sm:w-[45%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-700" : 
                                 "md:hidden fixed left-[-100%] top-0 w-[75%] sm:w-[45%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-700"}>
                     <div className="flex justify-between item-center mb-10">
-                        <NavLink to="/" className="text-2xl">Blogger App</NavLink>
+                        <NavLink to="/" className="text-2xl" onClick={handleNav}>Blogger App</NavLink>
                         <div className="cursor-pointer" onClick={handleNav}>
                             <AiOutlineClose size={25}/>
                         </div>
@@ -53,6 +53,7 @@ const Header = () => {
                     <ul className="flex flex-col gap-10">
                         <NavLink 
                             to="allnotes" 
+                            onClick={handleNav}
                             className={({ isActive }) =>
                             isActive ?
                             "underline cursor-default" : 
@@ -60,6 +61,7 @@ const Header = () => {
                         }>All Notes</NavLink> 
                         <NavLink 
                             to="newnote" 
+                            onClick={handleNav}
                             className={({ isActive }) =>
                             isActive ? 
                             "underline cursor-default" : 
@@ -67,6 +69,7 @@ const Header = () => {
                         }>New Note</NavLink>
                         <NavLink 
                             to="aboutus" 
+                            onClick={handleNav}
                             className={({ isActive }) =>
                             isActive ? 
                             "underline cursor-default" : 
